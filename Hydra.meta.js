d955752ba40d10f85093967e68c99537ad70a436
eddobrev <eddobrev@amazon.com>
2026-07-29 15:24:38 +0000

    chore(hydra): Bump version to 2.25
    cr: https://code.amazon.com/reviews/CR-292976991

--- Hydra.meta.js
+++ Hydra.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Hydra
-// @version      2.24
+// @version      2.25
 // @description  NASC Ops Chase Tool
 // @author       eddobrev
 // @updateURL    https://code.amazon.com/packages/HydraUserscript/blobs/mainline/--/Hydra.meta.js?raw=1
