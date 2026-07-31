61c1ede75dd1a6d3b7fae180a7b379f52ba1775e
eddobrev <eddobrev@amazon.com>
2026-07-31 17:27:14 +0000

    chore: Bump version to 3.37

--- Hydra.meta.js
+++ Hydra.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Hydra
-// @version      3.36
+// @version      3.37
 // @description  NASC Ops Chase Tool
 // @author       eddobrev
 // @updateURL    https://raw.githubusercontent.com/edobre4/Hydra/main/Hydra.meta.js
