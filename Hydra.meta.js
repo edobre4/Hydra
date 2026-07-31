3f96a44d547bafc06b300949e8231d1e5250c06b
eddobrev <eddobrev@amazon.com>
2026-07-31 13:04:59 +0000

    chore: Bump to v2.31 - test auto-update via code.amazon.com
    cr: https://code.amazon.com/reviews/CR-293520576

--- Hydra.meta.js
+++ Hydra.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Hydra
-// @version      2.30
+// @version      2.31
 // @description  NASC Ops Chase Tool
 // @author       eddobrev
-// @updateURL    https://raw.githubusercontent.com/edobre4/Hydra/main/Hydra.meta.js
-// @downloadURL  https://raw.githubusercontent.com/edobre4/Hydra/main/Hydra.user.js
+// @updateURL    https://code.amazon.com/packages/HydraUserscript/blobs/mainline/--/Hydra.meta.js?raw=1
+// @downloadURL  https://code.amazon.com/packages/HydraUserscript/blobs/mainline/--/Hydra.user.js?raw=1
 // @match        https://trans-logistics.amazon.com/ssp/dock/hrz/ib*
 // @match        https://trans-logistics.amazon.com/ssp/dock/ib*
 // @match        https://trans-logistics.amazon.com/ssp/dock/hrz/ob*
